38e74f82e39602184b8955d78390fc976026f4c6
ccarroll91 <conor-carroll@hotmail.com>
2021-03-10 14:34:21 +0000

    Filled contact page with form and script in head, created seperate js file for contact page and email response

---
+++ assets/js/script-contact.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "contact", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response)
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+        return false;
+}
